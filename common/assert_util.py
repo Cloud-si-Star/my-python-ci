50ac513fb3e1a392153487c72a5a6c155fca80b2
Luosi <2064382814@qq.com>
2026-09-14 22:01:40 +0800

    个人测试新增

--- common/assert_util.py
+++ common/assert_util.py
@@ -2,7 +2,6 @@
 def assert_status_code(resp,expected):
     assert resp.status_code==expected,f"状态码错误：期望：{expected}，实际：{resp.status_code}"
 
-
 def assert_json_value(resp,key,expected):
     actual=resp.json().get(key)
     assert actual==expected,f"字段{key}错误，期望{expected}，实际{actual}"
@@ -11,3 +10,16 @@ def assert_json_value(resp,key,expected):
 def assert_key_exists(resp,key):
     assert key in resp.json(),f"响应中不存在字段{key}"
 
+
+# 断言封装-验证状态码
+def assert_http_code(resp,expected):
+    assert resp.status_code==expected,f"状态码错误，期望：{expected}|实际：{resp.status_code}"
+
+# 断言封装-判定某个值是否相等
+def assert_value_is_expected(resp,key,expected):
+    acvalue=resp.json().get(key)
+    assert acvalue==expected,f"对应值错误，期望：{expected}|实际：{acvalue}"
+
+def assert_value_is_exits(resp,key,extis):
+    acvalue=resp.json().get(key)
+    assert extis in acvalue,f"{key}对应value值中不存在{extis}"
--- testcases/test_user.py
+++ testcases/test_user.py
@@ -1,5 +1,6 @@
 import pytest
-from common.assert_util import assert_status_code,assert_key_exists,assert_json_value
+from common.assert_util import assert_status_code,assert_key_exists,assert_json_value,assert_http_code,assert_value_is_expected,assert_value_is_exits
+from common.yaml_util import read_yaml
 
 def test_get_user(api,login_token):
     resp=api.get("/posts/1")
@@ -16,3 +17,32 @@ def test_create_post(api,login_token):
     })
     assert_status_code(resp,200)
     assert_key_exists(resp,"id")
+
+@pytest.mark.alone
+def test_alone_get(api,login_token):
+    resp=api.get("/users/1")
+    assert_http_code(resp,200)
+    assert_value_is_expected(resp,"name","Leanne Graham")
+    assert_value_is_exits(resp,"email","@")
+
+
+# 取数数据
+post_data=read_yaml("data/param_data.yaml").get("post_case")
+@pytest.mark.alone
+@pytest.mark.parametrize(
+    "case",
+post_data,
+    ids=[]
+)
+def test_alone_post(api,login_token,case):
+
+    resp=api.post("/posts",json={
+        "title":"pytest artical",
+        "body":"This is pytest partical",
+        "userId":10
+    })
+    print(resp.status_code)
+    assert_http_code(resp,case["code"])
+    assert_key_exists(resp,case["key_exits"])
+    assert_value_is_expected(resp,"userId",case["userId"])
+    assert_value_is_exits(resp,"title",case["text_exits"])
